5c6a421c25a21a318e3b9f502232fd13c412a20e
alsonentuna <alson.entuna@outlook.com>
2022-07-04 17:05:36 +0800

    - adding initial files

---
+++ SendWhatsAppMessage/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SendWhatsAppMessage
+{
+    class Program
+    {
+        private static string _whatsAppUri = "https://api.whatsapp.com/send/?phone=";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter WhatsApp No.:");
+            string number = Console.ReadLine();
+            string whatsAppUrl = $"{_whatsAppUri}{number}";
+            var psi = new ProcessStartInfo();
+            psi.UseShellExecute = true;
+            psi.FileName = whatsAppUrl;
+            Process.Start(psi);
+        }
+    }
+}
